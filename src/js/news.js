f149bfdd187b534e660678ccccb72961192a1079
Cezar Viana <cezar.viana@gmail.com>
2025-04-01 18:07:19 -0300

    <add>: responsive to index, news and mjer

--- src/js/news.js
+++ src/js/news.js
@@ -3,7 +3,7 @@ let newsS = [
         title : "Primeiro brasileiro a alcançar o 7º Dan em Iaido",
         image : "../src/images/photos/photo-igarashi-alexandre-other.jpg",
         imageAlt : "Foto retirada após o exame de 7º Dan, da esquerda para a direita, Igarashi, Alexandre, sem nome",
-        imageTitle :"Da esquerda para a direita, Igarashi, Alexandre, sem nome",
+        imageTitle :"Foto retirada após o exame de 7º Dan, com Alexandre Pereira ao centro",
         date : "02/03/2025",
         text : "No dia 02 de março de 2025, em Quioto, o Sensei Alexandre Pereira alcançou o 7º Dan Renshi Muso Jikiden Eishin Ryu, é o brasileiro com a maior graduação pela Federação Japonesa de Kendo (Zen Nihon Kendo Renmei - ZNKR). Um marco significativo para as artes marciais e o Iaido no Brasil. A conquista, que exige anos de dedicação, habilidade e conhecimento profundo, consolida a posição do Sensei Alexandre como um respeitado expoente da modalidade, sendo o primeiro brasileiro a alcançar esse patamar. O feito foi celebrado por seus alunos e colegas.",
         link : "",
